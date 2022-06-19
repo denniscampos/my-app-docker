b5796c87b19c31a08a48e0e7456d7d1d187446ae
Dennis Campos <dennisjr89@gmail.com>
2022-06-19 13:06:39 -0400

    feat: add docker

--- api/server.js
+++ api/server.js
@@ -1,14 +1,18 @@
-import express from 'express';
+import express from "express";
 
-const PORT = process.env.PORT || 6969;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.get('/api', (req, res) => {
-    res.status(200).json({message: 'hello world!! Im from the backend!!!'})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World! 🔥" });
+});
+
+app.get("/api", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "hello world!! Im from the backend!!! HE HE HA HA" });
+});
 
 app.listen(PORT, (req, res) => {
-    console.log(`Server running on ${PORT}`)
-})
-
-// next step is to add docker :D 
+  console.log(`Server running on ${PORT}`);
+});
--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+// import logo from './logo.svg';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('/api')
-      .then((res) => res.json()).then((data) => setData(data.message))
-  }, [])
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+      .catch((err) => console.log("error: ******** ", err));
+  }, []);
 
   return (
     <div className="App">
       <h1>My default is better than react 🤷🏼‍♂️</h1>
+      <p>hi</p>
       {data}
     </div>
   );
